f3470167a4a6cc34cc982063d0e6ec8d9a8f69bf
duyd486 <90164590+duyd486@users.noreply.github.com>
2025-11-01 15:25:28 +0700

    update core mechanic

---
+++ Assets/MyGame/Scripts/Player/TestProjection.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class TestProjection : MonoBehaviour
+{
+    public Transform playerCam;
+    public Transform photoPlane;
+    public Transform objectPlane;
+    public float finalDistance = 20f;
+    private float initialDistance;
+
+    private Vector3 initialPos;
+    private Vector3 initialScale;
+
+    void Start()
+    {
+        initialPos = photoPlane.position;
+        initialScale = photoPlane.localScale;
+        initialDistance = Vector3.Distance(playerCam.position, initialPos);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            ExpandToWorld();
+        }
+    }
+
+    public void ExpandToWorld()
+    {
+        objectPlane.rotation = photoPlane.rotation;
+        photoPlane.gameObject.SetActive(false);
+
+        float scaleFactor = finalDistance / initialDistance;
+        objectPlane.localScale = initialScale * scaleFactor;
+
+        objectPlane.position = playerCam.position + playerCam.forward * finalDistance;
+    }
+}
